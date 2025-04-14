4a8308ee44cc21da86dfe044d1cc33aebc61a462
Shawoor Ayyoob <mohdshawoor@gmail.com>
2025-04-14 11:08:18 +0530

    14th April

--- Palindrome.java
+++ Palindrome.java
@@ -1,14 +1,31 @@
+// Step 1 - reverse Number
+// Step 2- Compare the orginal with reversed number.
+
 public class Palindrome {
     public static void main(String[] args) {
-        printPalindrome(204);
+        int orignalNo = 12321;
+        int reversedNo = reverse(orignalNo);
+        compareNo(orignalNo, reversedNo);
     }
 
-    public static void printPalindrome(int n) {
-        int remainder = n * 10;
-        int palindromeNo = n;
-        while (remainder != 0) {
+    public static int reverse(int n) {
+        int remainder;
+        int reversedNo = 0;
+        while (n != 0) {
+            remainder = n % 10;
             n = n / 10;
+            reversedNo = reversedNo * 10 + remainder;
         }
-        System.out.println(" Palindrome of " + palindromeNo + " = " + n);
+        return reversedNo;
+        // System.out.println(" Palindrome of " + orignalNo + " = " + reversedNo);
+    }
+
+    public static void compareNo(int orignalNo, int reversedNo) {
+        if (orignalNo == reversedNo) {
+            System.out.println(orignalNo + " is a palindrome");
+        } else {
+            System.out.println(orignalNo + " is not a palindrome");
+
+        }
     }
 }
