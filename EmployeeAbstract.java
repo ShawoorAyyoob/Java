54f07d3943666a7889bca320bcc0bfc9cdd1756c
Shawoor Kasim <mohdshawoor@gmail.com>
2025-04-28 23:08:22 +0530

    28th Asg

--- EmployeeAbstract.java
+++ EmployeeAbstract.java
@@ -1,5 +1,16 @@
 public class EmployeeAbstract {
     public static void main(String[] args) {
+        Employee employee1 = new FullTimeEmployee();
+        Employee employee2 = new PartTimeEmployee();
+
+        employee1.calculateSalary();
+        employee1.displayDetails();
+        employee2.calculateSalary();
+        employee2.displayDetails();
+
+    }
+
+    void printEmployee(Employee emp) {
         
     }
 }
@@ -8,20 +19,32 @@ abstract class Employee {
     protected String name;
     protected int employeeId;
 
-    abstract void double calculateSalary();
+    abstract void calculateSalary();
 
     void displayDetails() {
+        System.out.println("*** Employee Details ***");
         System.out.println("Employee ID: " + employeeId);
         System.out.println("Name: " + name);
-        System.out.println("Salary: " + salary);
     }
 }
 
 class FullTimeEmployee extends Employee {
     protected double monthlySalary;
-    
+
     @Override
     void calculateSalary() {
+        System.out.println(monthlySalary);
+        return;
+    }
+}
+
+class PartTimeEmployee extends Employee {
+    protected double hourlyWage;
+    protected int hoursWorked;
+
+    @Override
+    void calculateSalary() {
+        System.out.println(hourlyWage * hoursWorked);
         return;
     }
 }
